0aedf7bad206f2b2fd94a3ec9461430079263c9a
Ivan Valenzuela <ivalenzuela@ntic.cl>
2022-09-06 13:36:58 -0300

    s

--- PartnerProgram.sol
+++ PartnerProgram.sol
@@ -16,8 +16,10 @@ contract PartnerProgram is Context {
 
     struct Project {
         address contractAddress;
+        address contractOwner;
         address payable paymentsWallet;
         uint256 partnerCommission;
+        uint256 partnerPremiumCommission;
         address author;
         string coinName;
         string coinSymbol;
@@ -41,8 +43,9 @@ contract PartnerProgram is Context {
     mapping(string => Partner) public partners;
     mapping(address => Project) public projects;
 
-    event Deposit(address indexed sender, uint amount);
+    event Deposit(address sender, uint amount);
     event NewPartner(string name, string code);
+    event UpdatePartner(string name, string code);
     event NewProject(address contractAddress, string _coinName, string _coinSymbol, string website);
     event PartnerProgramBUY(address indexed sender, address indexed _contract, string indexed _code, uint amount);
 
@@ -99,7 +102,7 @@ contract PartnerProgram is Context {
         _;
     }
 
-    function depositTokens(uint _amount, string memory _code, address _contractAddress) external {
+    function partnerProgramBUYTokens(uint _amount, string memory _code, address _contractAddress) external {
         require(partners[_code].isValue, "Partner Program BUY: code not exist");
         require(_amount > 0, "PartnerProgramBUY: You deposit send at least some tokens");
         IERC20 _token = IERC20(_contractAddress);
@@ -110,7 +113,7 @@ contract PartnerProgram is Context {
         emit PartnerProgramBUY(_msgSender(), _contractAddress, _code, _amount);
     }
 
-    function depositETH(string memory _code, address _contractAddress) external payable isPartnerProgramMember(_code) isPartnerProgramContract(_contractAddress) {
+    function partnerProgramBUY(string memory _code, address _contractAddress) external payable isPartnerProgramMember(_code) isPartnerProgramContract(_contractAddress) {
         require(msg.value > 0, "You need to send some ether");
         executePaymentsETH(_contractAddress, _code);
         emit PartnerProgramBUY(_msgSender(), _contractAddress, _code, msg.value);
@@ -120,21 +123,24 @@ contract PartnerProgram is Context {
         address _contractAddress,
         address payable _paymentsWallet,
         uint256 _partnerCommission,
+        uint256 _partnerPremiumCommission,
         string memory _coinName,
         string memory _coinSymbol,
         string memory _website,
         string memory _twitter,
         string memory _telegram,
         string memory _discord) external {
-        require(msg.sender == tx.origin, "New Project: projects not allowed here");
+        require(msg.sender == tx.origin, "New Project: contracts not allowed here");
         require(_partnerCommission > 0, "New Project: commission must be greater than zero");
         require(_partnerCommission <= 30, "New Project: partner commission must keep 30% or less");
         IERC20 _token = IERC20(_contractAddress);
         require(_token.owner() == _msgSender(), "New Project: caller is not the owner");
         projects[_contractAddress] = Project({
         contractAddress : _contractAddress,
+        contractOwner: msgSender(),
         paymentsWallet : _paymentsWallet,
         partnerCommission : _partnerCommission,
+        partnerPremiumCommission : _partnerPremiumCommission,
         author : _msgSender(),
         coinName : _coinName,
         coinSymbol : _coinSymbol,
@@ -145,6 +151,41 @@ contract PartnerProgram is Context {
         isValue : true
         });
         emit NewProject(_contractAddress, _coinName, _coinSymbol, _website);
+    }
+    function updateProject (
+        address _contractAddress,
+        address payable _paymentsWallet,
+        address payable _paymentsWallet,
+        uint256 _partnerCommission,
+        uint256 _partnerPremiumCommission,
+        string memory _coinName,
+        string memory _coinSymbol,
+        string memory _website,
+        string memory _twitter,
+        string memory _telegram,
+        string memory _discord) external {
+        require(msg.sender == tx.origin, "Update Project: contracts not allowed here");
+        require(msg.sender == tx.origin, "Update Project: projects not allowed here");
+        require(_partnerCommission > 0, "Update Project: commission must be greater than zero");
+        require(_partnerCommission <= 30, "New Project: partner commission must keep 30% or less");
+
+        IERC20 _token = IERC20(_contractAddress);
+        require(_token.owner() == _msgSender(), "New Project: caller is not the owner");
+        projects[_contractAddress] = Project({
+        contractAddress : _contractAddress,
+        paymentsWallet : _paymentsWallet,
+        partnerCommission : _partnerCommission,
+        partnerPremiumCommission : _partnerPremiumCommission,
+        author : _msgSender(),
+        coinName : _coinName,
+        coinSymbol : _coinSymbol,
+        website : _website,
+        twitter : _twitter,
+        telegram : _telegram,
+        discord : _discord,
+        isValue : true
+        });
+        emit updateProject(_contractAddress, _coinName, _coinSymbol, _website);
     }
 
     function joinAsPartner(
@@ -168,12 +209,51 @@ contract PartnerProgram is Context {
         emit NewPartner(_name, _code);
     }
 
-    function recoverERC20(address _contractAddress, uint256 _amount) external {
+    function updatePartner(
+        string memory _name,
+        string memory _code,
+        address payable  _partnerAddress,
+        address payable _managerAddress,
+        uint256 _taxFeePartner,
+        uint256 _taxFeeManager) external {
+        require(!partners[_code]._partnerAddress == _msgSender() , "Partners: only Partner can change the data");
+        require(!partners[_code].isValue, "Partners: code already exists");
+        require(_taxFeePartner + _taxFeeManager == 100, "The sum of the taxes must be 100");
+        partners[_code] = Partner({
+        name : _name,
+        code : _code,
+        partnerAddress : _partnerAddress,
+        managerAddress : _managerAddress,
+        taxFeePartner : _taxFeePartner,
+        taxFeeManager : _taxFeeManager,
+        isValue : true
+        });
+        emit UpdatePartner(_name, _code);
+    }
+    function updatePartner(
+        string memory _name,
+        string memory _code,
+        address payable  _partnerAddress,
+        address payable _managerAddress,
+        uint256 _taxFeePartner,
+        uint256 _taxFeeManager) external {
+        require(!partners[_code]._partnerAddress == _msgSender() , "Partners: only Partner can change the data");
+        require(!partners[_code].isValue, "Partners: code already exists");
+        require(_taxFeePartner + _taxFeeManager == 100, "The sum of the taxes must be 100");
+        partners[_code] = Partner({
+        name : _name,
+        code : _code,
+        partnerAddress : _partnerAddress,
+        managerAddress : _managerAddress,
+        taxFeePartner : _taxFeePartner,
+        taxFeeManager : _taxFeeManager,
+        isValue : true
+        });
+        emit UpdatePartner(_name, _code);
+    }
+    function ownerPayment(address _contractAddress, uint256 _amount) external {
         IERC20 _token = IERC20(_contractAddress);
         _token.transfer(partnerProgramOwner, _amount);
-    }
-
-    function recoverETH() external {
         (bool sent,) = partnerProgramOwner.call{value : address(this).balance}("");
         require(sent, "recover ETH: Failed to send ETH");
     }
